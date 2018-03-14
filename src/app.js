ae4875fca93921994eaeb5129da0f3287f247de6
Matt Atwell <ma3505@rit.edu>
2018-03-14 12:35:29 -0400

    Convert Project to Browserify

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log("Demo works");
+var $ = require('jquery');
+var dgram = require('dgram');
+$(document).ready(()=>{
+  // Launch Ajax call to PHP server
+  $(".connect").click(()=>{
+    console.log("click");
+  });
+
+});
