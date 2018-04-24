3b83369df3f4e8daaea14b0b479eafb5c24e80b5
Matt Atwell <ma3505@rit.edu>
2018-04-23 23:52:50 -0400

    Add whitespace around messages

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const url = require('url')
 
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 880})
 
     // and load the index.html of the app.
     win.loadURL(url.format({
